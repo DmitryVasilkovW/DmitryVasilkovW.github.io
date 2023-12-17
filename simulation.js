68572bfb1a8c5df4e8927e47a41f33ee907b0aa5
Dmitry Vasilkov <113594620+DmitryVasilkovW@users.noreply.github.com>
2023-12-17 15:21:48 +0300

    fix: formulas

--- simulation.js
+++ simulation.js
@@ -3,10 +3,10 @@ function calculate() {
     const v_prime = parseFloat(document.getElementById('v_prime').value);
     const x_prime = parseFloat(document.getElementById('x_prime').value);
 
-    const v = Math.round(((V + v_prime) / (1 + V * v_prime)) * 10) / 10;
-    const x = Math.round((V * x_prime / (1 + V * v_prime)) * 100) / 100;
+    const v = V + v_prime;
+    const x = x_prime + V;
 
-    document.getElementById('output').innerHTML = 'Скорость v: ' + v + ', Координата x: ' + x;
+    document.getElementById('output').innerHTML = 'Скорость v: ' + v.toFixed(2) + ' м/с, Координата x: ' + x.toFixed(2) + ' м';
 }
 
 function calculatePrime() {
@@ -14,8 +14,8 @@ function calculatePrime() {
     const v = parseFloat(document.getElementById('v').value);
     const x = parseFloat(document.getElementById('x').value);
 
-    const v_prime = Math.round((v + V * (v ** 2) - V) * 100) / 100;
-    const x_prime = Math.round(((x * (1 + V * v_prime)) / V) * 100) / 100;
+    const v_prime = v - V;
+    const x_prime = x - V;
 
-    document.getElementById('output').innerHTML = "Скорость v': " + v_prime + ", Координата x_0': " + x_prime;
+    document.getElementById('output').innerHTML = "Скорость v': " + v_prime.toFixed(2) + " м/с, Координата x<sub>0</sub>': " + x_prime.toFixed(2) + " м";
 }
