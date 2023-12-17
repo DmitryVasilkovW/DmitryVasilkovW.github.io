714a888ff87ec2d65846a138100f3708c3ab850b
Dmitry Vasilkov <113594620+DmitryVasilkovW@users.noreply.github.com>
2023-12-17 17:17:46 +0300

    fix: all

--- simulation.js
+++ simulation.js
@@ -4,6 +4,7 @@ function calculate() {
     const V = parseFloat(document.getElementById('V').value);
     const v_prime = parseFloat(document.getElementById('v_prime').value);
     const x_prime = parseFloat(document.getElementById('x_prime').value);
+    const t_prime = parseFloat(document.getElementById('t_prime').value);
 
     if (isNaN(V)) {
         alert("Пожалуйста, введите значение для скорости системы отсчета V (м/с).");
@@ -20,14 +21,19 @@ function calculate() {
         return;
     }
 
+    if (isNaN(t_prime)) {
+        alert("Пожалуйста, введите значение для времени t' (с).");
+        return;
+    }
+
     if (V >= c || v_prime >= c) {
         alert("Введенные значения не должны быть релятивистскими.");
         return;
     }
 
     const v = V + v_prime;
-    const x = x_prime + V;
-    const t = x / v + x_prime;
+    const x = x_prime + V * t_prime;
+    const t = t_prime;
 
     document.getElementById('output').innerHTML = 'Скорость v: ' + v.toFixed(2) + ' м/с, Координата x: ' + x.toFixed(2) + ' м, Время t: ' + t.toFixed(2) + ' с';
 }
@@ -36,6 +42,7 @@ function calculatePrime() {
     const V = parseFloat(document.getElementById('V2').value);
     const v = parseFloat(document.getElementById('v').value);
     const x = parseFloat(document.getElementById('x').value);
+    const t = parseFloat(document.getElementById('t').value);
 
     if (isNaN(V)) {
         alert("Пожалуйста, введите значение для скорости системы отсчета V (м/с).");
@@ -52,13 +59,19 @@ function calculatePrime() {
         return;
     }
 
+    if (isNaN(t)) {
+        alert("Пожалуйста, введите значение для времени t (с).");
+        return;
+    }
+
     if (V >= c || v >= c) {
         alert("Введенные значения не должны быть релятивистскими.");
         return;
     }
 
     const v_prime = v - V;
-    const x_prime = x - V;
+    const x_prime = x - V * t;
+    const t_prime = t;
 
-    document.getElementById('output').innerHTML = "Скорость v': " + v_prime.toFixed(2) + " м/с, Координата x<sub>0</sub>': " + x_prime.toFixed(2);
+    document.getElementById('output').innerHTML = "Скорость v': " + v_prime.toFixed(2) + " м/с, Координата x<sub>0</sub>': " + x_prime.toFixed(2) + " м, Время t': " + t_prime.toFixed(2) + " с";
 }
