a664568c1d664d3a16761e8d4f708d8c157483d6
Dmitry Vasilkov <113594620+DmitryVasilkovW@users.noreply.github.com>
2024-07-08 00:47:39 +0300

    feat: added the ability to dynamically expand project description cards

--- my-portfolio/src/data/projects.ts
+++ my-portfolio/src/data/projects.ts
@@ -1,23 +1,3 @@
-const projects = [
-    {
-        id: 1,
-        title: 'Personal Project 1',
-        description: 'Description of Personal Project 1',
-        tags: ['Personal', 'React'],
-    },
-    {
-        id: 2,
-        title: 'Project at ITMO',
-        description: 'Description of Project at ITMO',
-        tags: ['ITMO'],
-    },
-    {
-        id: 3,
-        title: 'Personal Project 2',
-        description: 'Description of Personal Project 2',
-        tags: ['Personal', 'JavaScript'],
-    },
-    // Add more projects as needed
-];
+import projects from './projects/index';
 
 export default projects;
